c4928c490ae843fe248b5eb0801de1712ac72660
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-11-29 22:16:17 -0800

    first commit

---
+++ 0x01-python-if_else_loops_functions/list.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*/
+
+typedef struct listint_s
+{
+
+	int n;
+	struct listint_s *next;
+}
+listint_t;
+
+/* Function prototypes */
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
