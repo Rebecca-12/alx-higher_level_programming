d638a1919077fe4c1b6cf95532e06b0c2906c7d9
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-12-02 02:52:51 -0800

    Updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(i), end='')
-    i -= 1
-    if i >= ord('A'):
-        print("{:c}".format(i), end='')
+    if i % 2 == 0:
+        print(chr(i), end='')
+    else:
+        print(chr(i).upper(), end='')
 
 print()
