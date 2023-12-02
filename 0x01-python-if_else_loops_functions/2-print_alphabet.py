730e4e5e3efe4876a626417c9dfe758de629f5a5
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-12-01 21:22:32 -0800

    Updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# Task 2: Why was the math book sad?
-print("Why was the math book sad?")
-print("Because it had too many problems!")
+for i in range(97, 123):
+    print(chr(i), end='')
