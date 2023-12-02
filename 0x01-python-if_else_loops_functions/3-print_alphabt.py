8f72f494dc6f1431c6396b22e4d67616595a6063
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-12-01 21:46:15 -0800

    Updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-# Task 3: How does a penguin build its house?
-print("How does a penguin build its house?")
-print("Igloos it together!")
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print("{}".format(chr(i)), end='')
